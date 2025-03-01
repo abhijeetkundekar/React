b4b71f78c822dc154e9b0e84fbfee2a896457b3c
Abhijeet <abhi@botzee.com>
2025-03-01 21:41:19 +0530

    check

---
+++ tailwind-app/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './Components/NavBar';
+
+function App() {
+  return (
+    <div>
+       <h1 className="text-xl text-blue-400 font-bold">
+      Hello world!
+    </h1>
+    <NavBar></NavBar>
+    </div>
+  );
+}
+
+export default App;
